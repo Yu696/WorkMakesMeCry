0fbd7c7ec1675e25e77be44b7db3c0bca0b7e642
Yu696 <1804537366@qq.com>
2025-06-06 12:30:17 +0800

    提交1补充

--- src/main/java/com/neuedu/nep/controller/HelloCotroller.java
+++ src/main/java/com/neuedu/nep/controller/HelloCotroller.java
@@ -1,4 +1,23 @@
 package com.neuedu.nep.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class HelloCotroller {
+    @FXML
+    private Button registerButton;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    public void registerButtonClicked(){
+        registerButton.setText("已点击");
+    }
+
+    @FXML
+    public void loginButtonClicked(){
+        loginButton.setText("已点击");
+    }
+
 }
